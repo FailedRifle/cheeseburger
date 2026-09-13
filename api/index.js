fdf944dcf40897e8ebeccaa99a1e55279ee1aac6
cheddar <139413504+FailedRifle@users.noreply.github.com>
2026-09-13 10:16:51 -0400

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import { fastify } from "../src/index.js";
-
-let ready;
-
-export default async function handler(request, response) {
-  ready ??= fastify.ready();
-  await ready;
-  fastify.server.emit("request", request, response);
-}
